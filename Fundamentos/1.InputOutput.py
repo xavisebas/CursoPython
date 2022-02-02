2fc143e599e84a8aaf5986a821e2c5da996fbec7
xavisebas <eduardoguaygua@gmail.com>
2022-02-01 20:00:36 -0500

    variables

---
+++ Fundamentos/1.InputOutput.py
@@ -0,0 +1,24 @@
+#Function entrada y salida
+#Va a buscar las palabras reservadas
+
+#Arguneto a lafuncion, ''->string
+print('Formulario')
+print('Nombre: ',end='')
+#inout -> Entrada de datos
+nombre = input()
+print('Apellido: ',end='')
+apellido = input()
+print('Edad: ',end='')
+edad = input()
+
+#primer reto
+ciudad = input('Ingresa tu ciudad:')
+
+print('El nombre ingresado es: ',nombre)
+print('El apellido ingresado es: ',apellido)
+print('La edad ingresada es: ',edad)
+print('Tu ciudad ingresada es: ',ciudad)
+
+
+
+#python es un lenguaje 100% Orientado a Objetos
---
+++ Fundamentos/2.Variables.py
@@ -0,0 +1,17 @@
+#variables
+#No deben tener caracteres especiales, no empear conmayusculas,no palabras reservadas
+#Tipos de variables
+#python es un lenguaje de tipado debil
+#El tipo de dato de las variables viene dado  por su iniciliacion
+#python hace un cast-> casting de variables
+
+edad = 23 #int
+nombre='Anderson'
+#la declaracion de flotante es con un punto
+ira=37.2
+mayorEdad = True
+
+print('El tipo de dato de lavariable edad es:',type(edad))
+print('El tipo de dato de lavariable edad es:',type(nombre))
+print('El tipo de dato de lavariable edad es:',type(ira))
+print('El tipo de dato de lavariable edad es:',type(mayorEdad))
