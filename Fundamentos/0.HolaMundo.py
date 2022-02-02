a05d085ba3bef181762b961f31cb1e1c4caea1ce
xavisebas <eduardoguaygua@gmail.com>
2022-02-01 19:00:33 -0500

    inicio

---
+++ Fundamentos/0.HolaMundo.py
@@ -0,0 +1,15 @@
+#Nuestro 1er Script
+'''
+es
+un
+cometario
+largo
+'''
+
+#Entrada de datos con input
+#Salida de datos con print
+#no diferencia comillas simples o dobles
+print('Formulario')
+print('Ingresa tu nombre: ')
+nombre = input()
+print('El nombre ingresado es:',nombre)
