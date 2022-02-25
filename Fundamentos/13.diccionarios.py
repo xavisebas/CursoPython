277a95f95db281575412c684b689d833c1a0587b
xavisebas <eduardoguaygua@gmail.com>
2022-02-25 16:02:10 -0500

    update8

--- Fundamentos/13.diccionarios.py
+++ Fundamentos/13.diccionarios.py
@@ -4,9 +4,7 @@
 # Tras la inicialización la clave es inmutable
 # Clave puede ser de cualquier tipo de dato al igual que el valor
 # No soporta la indexación
-
-from sklearn.metrics import precision_score
-
+# CRUD -< Create, update, read, delete
 
 diccionario1 = {
     'i1': 1,
@@ -42,18 +40,73 @@ print(persona[21])
 print(persona[llaveVariable])
 print(persona['materias'])
 
-
-listaPrueba = [0,1,2,3,4]
-
 print('Métodos')
 # obtener un valor
 print(persona.get('edad'))
 # actualizar un dato
 print(persona.update({'edad':23}))
+# agregar valor a un diccionario ya creado
+persona['universidad'] = 'EPN'
+print(persona)
+#eliminar valores de un diccionario
+# pop eliminar a través de la llave
+persona.pop(21)
+print(persona)
+# eliminación del valor 
+
+# 1. Saber cuales son solo los valores del diccionario
+print(persona.values())
+# 2. Saber cuales son solo las llaver del diccionario
+print(persona.keys())
+# 3. Diccionario mapeado
+print(persona.items())
+# 
+def obtenerLlave(valorBuscar):
+    for llave, valor in persona.items():
+        if valorBuscar == valor:
+            return llave
+        #return Exception
+
+print('Llave encontrada:',obtenerLlave('1726744814'))
+persona.pop(obtenerLlave('1726744814'))
 print(persona)
 
-# 
-#for i in listaPrueba:
-    #str(i): 
+# pop item -> eliminar el último valor insertado en el diccionario (3.7)
+persona.popitem()
+print(persona)
+
+# ¿Qué pasatía si pido obtener un valor ?
+print(persona.get('mascota', 'No existe este valor'))
+
+copiaPersona = persona.copy()
+print(copiaPersona)
+
+# Numerable
+print(len(copiaPersona))
+
+# Eliminar todo
+copiaPersona.clear()
+print(copiaPersona)
+
+# COnversión entre lista y diccionario
+# 1
+listaPrueba = [0,1,2,3,4]
+diccionarioPrueba = {}
+for i in listaPrueba:
+    diccionarioPrueba[i] = i
+
+print(diccionarioPrueba)
+
+# 2
+diccionarioPrueba2 = {}
+listaLlave = ['n0','n1','n2','n3','n4']
+listaPrueba2 = [0,1,2,3,4]
+for i in range(len(listaLlave)):
+    diccionarioPrueba2[listaLlave[i]] = listaPrueba2[i]
+print(diccionarioPrueba2)
+
+# 3
+diccionarioPrueba3 = dict(zip(listaLlave,listaPrueba2))
+print(diccionarioPrueba3)
 
 #diccionario10 = {'valor1':1,'valor2':2}
